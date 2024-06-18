e7762272b5d8bd9eeb6e6069793c837602666789
HongsCai <Hongs_Cai@outlook.com>
2024-06-18 16:08:31 +0800

    first commit

--- src/resources/sql/studata.sql
+++ src/resources/sql/studata.sql
@@ -24,17 +24,16 @@ CREATE TABLE `user_admin` (
 CREATE TABLE `stu` (
     `name` VARCHAR(20) NOT NULL,
     `gender` CHAR(2) NOT NULL,
-    `sno` CHAR(12) NOT NULL UNIQUE PRIMARY KEY,
+    `sno` CHAR(12) PRIMARY KEY,
     `major` VARCHAR(12) DEFAULT NULL
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- 创建 course 表
 CREATE TABLE `course` (
-    `cno` CHAR(4) NOT NULL,
+    `cno` CHAR(4) PRIMARY KEY,
     `cname` VARCHAR(20) DEFAULT NULL,
     `cteacher` VARCHAR(20) DEFAULT NULL,
-    `credit` INT(11) DEFAULT NULL,
-    PRIMARY KEY (`cno`) USING BTREE
+    `credit` INT(11) DEFAULT NULL
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- 创建 sc 表
@@ -48,11 +47,16 @@ CREATE TABLE `sc` (
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- 插入 user_admin 表数据
-INSERT INTO `user_admin` (`username`, `password`) VALUES ('admin', '123');
+INSERT INTO `user_admin` (`username`, `password`) VALUES
+('admin', '123');
+
 -- 插入 user_tch 表数据
-INSERT INTO `user_tch` (`username`, `password`) VALUES ('tch', '123');
+INSERT INTO `user_tch` (`username`, `password`) VALUES
+('tch', '123');
+
 -- 插入 user_stu 表数据
-INSERT INTO `user_stu` (`username`, `password`) VALUES ('stu', '123');
+INSERT INTO `user_stu` (`username`, `password`) VALUES
+('stu', '123');
 
 -- 插入 course 表数据
 INSERT INTO `course` (`cno`, `cname`, `cteacher`, `credit`) VALUES
@@ -64,7 +68,9 @@ INSERT INTO `course` (`cno`, `cname`, `cteacher`, `credit`) VALUES
 ('0006', '高等数学', '刘老师', 1),
 ('0007', '软件工程', '孙老师', 2),
 ('0008', '算法导论', '周老师', 3),
-('0009', 'Java面向对象程序设计', '吴老师', 4);
+('0009', 'Java面向对象程序设计', '吴老师', 4),
+('0010', '数据结构', '杨老师', 3),
+('0011', '计算机网络', '黄老师', 3);
 
 -- 插入 stu 表数据
 INSERT INTO `stu` (`name`, `gender`, `sno`, `major`) VALUES
@@ -74,7 +80,11 @@ INSERT INTO `stu` (`name`, `gender`, `sno`, `major`) VALUES
 ('刘慧敏', '女', '202220010404', '物联网工程'),
 ('陈光明', '男', '202220010405', '智能科学与技术'),
 ('吴小丽', '女', '202220010406', '智能科学与技术'),
-('林美玲', '女', '202220010407', '计算机科学与技术');
+('林美玲', '女', '202220010407', '计算机科学与技术'),
+('赵晓宇', '男', '202220010408', '软件工程'),
+('孙小芳', '女', '202220010409', '计算机科学与技术'),
+('马光辉', '男', '202220010410', '物联网工程'),
+('刘晓华', '女', '202220010411', '智能科学与技术');
 
 -- 插入 sc 表数据
 INSERT INTO `sc` (`sno`, `cno`, `score`) VALUES
@@ -92,4 +102,8 @@ INSERT INTO `sc` (`sno`, `cno`, `score`) VALUES
 ('202220010406', '0003', 90),
 ('202220010406', '0008', 92),
 ('202220010407', '0003', 85),
-('202220010407', '0009', 89);
+('202220010407', '0009', 89),
+('202220010408', '0010', 76),
+('202220010409', '0011', 84),
+('202220010410', '0010', 92),
+('202220010411', '0011', 88);
